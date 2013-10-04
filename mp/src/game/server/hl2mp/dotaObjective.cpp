6635e5c492fce1cf54b7966ca7d0bf06588188b4
NRacer <raceronline@yahoo.com>
2013-10-04 15:52:42 -0500

    +ConVar dotaLaneMode( "sv_dotaLaneMode", "-1", FCVAR_GAMEDLL | FCVAR_NOTIFY ); Issue #24: Conditionally open lanes

--- mp/src/game/server/hl2mp/dotaObjective.cpp
+++ mp/src/game/server/hl2mp/dotaObjective.cpp
@@ -21,6 +21,8 @@ BEGIN_DATADESC( DotaObjective )
 	
 END_DATADESC()
 
+ConVar dotaLaneMode( "sv_dotaLaneMode", "-1", FCVAR_GAMEDLL | FCVAR_NOTIFY ); // Issue #24: AMP - 2013-10-04 - Conditionally open lanes
+
 DotaObjective::DotaObjective(void)
 {
 	m_timesHit = 0;
@@ -30,8 +32,9 @@ DotaObjective::DotaObjective(void)
 void DotaObjective::Spawn( )
 {
 	BaseClass::Spawn();
-
-	PropSetAnim( "Open" );
+	if (CheckLaneMode()) {
+		TakeAction(DOBJ_ACTION_OPEN);
+	}
 }
 
 int DotaObjective::OnTakeDamage( const CTakeDamageInfo &inputInfo )
@@ -61,7 +64,7 @@ int DotaObjective::OnTakeDamage( const CTakeDamageInfo &inputInfo )
 			user.AddRecipientsByTeam( this->GetTeam() );
 			user.MakeReliable();
 			char szText[200];
-			Q_snprintf( szText, sizeof(szText), "Your aAlly gate is under attack from an enemy!" );
+			Q_snprintf( szText, sizeof(szText), "Your ally gate is under attack from an enemy!" );
 			UTIL_ClientPrintFilter( user, HUD_PRINTCENTER, szText );
 			
 			if( playerAttacker )
@@ -69,33 +72,81 @@ int DotaObjective::OnTakeDamage( const CTakeDamageInfo &inputInfo )
 
 			if (m_timesHit >= 30)
 			{
-				PropSetAnim( "Close" );
-
-				m_bMet = true;			
-				
-				//turn off the maker
-				if( maker )
-					maker->m_nMaxLiveChildren = 0;
-
-				//see if I'm the last one, if so, game over
-				bool foundOne = false;
-				for ( CBaseEntity *pEntity = gEntList.FirstEnt(); pEntity != NULL; pEntity = gEntList.NextEnt(pEntity) )
-				{
-					DotaObjective * obj = dynamic_cast<DotaObjective*>( pEntity );
-					if ( obj && obj->GetTeamNumber() == this->GetTeamNumber() && !obj->m_bMet )
-						foundOne = true;
-				}
-				if ( !foundOne )
-				{
-					CHL2MPRules *rules = (CHL2MPRules*)g_pGameRules;
-					if ( rules && !rules->IsIntermission() )
-					{
-						rules->EndMultiplayerGame();
-					}
-				}
+				TakeAction(DOBJ_ACTION_CLOSE);
 			}
 		}
 	}
 
 	return 0;
+}
+
+// Ability to alter the objective:
+//   DOBJ_ACTION_OPEN: Opens the objective door and starts spawning creeps (if round has started)
+//   DOBJ_ACTION_CLOSE: Closes the objective door and stops spawning creeps (if this is the last door, the round will end)
+int DotaObjective::TakeAction( int dobjAction ) { // Issue #24: AMP - 2013-10-04 - Lane manipulation
+	if (dobjAction & DOBJ_ACTION_CLOSE) {
+		PropSetAnim( "Close" );
+
+		m_bMet = true;			
+				
+		//turn off the maker
+		CreepMaker * maker = (CreepMaker*)gEntList.FindEntityByName( NULL, m_creepMakerName );
+		if( maker )
+			maker->m_nMaxLiveChildren = 0;
+
+		//see if I'm the last one, if so, game over
+		bool foundOne = false;
+		for ( CBaseEntity *pEntity = gEntList.FirstEnt(); pEntity != NULL; pEntity = gEntList.NextEnt(pEntity) )
+		{
+			DotaObjective * obj = dynamic_cast<DotaObjective*>( pEntity );
+			if ( obj && obj->GetTeamNumber() == this->GetTeamNumber() && !obj->m_bMet )
+				foundOne = true;
+		}
+		if ( !foundOne )
+		{
+			CHL2MPRules *rules = (CHL2MPRules*)g_pGameRules;
+			if ( rules && !rules->IsIntermission() )
+			{
+				rules->EndMultiplayerGame();
+			}
+		}
+		return true;
+	} else if (dobjAction & DOBJ_ACTION_OPEN) {
+		m_timesHit = 0;
+		m_bMet = false;
+		CreepMaker * maker = (CreepMaker*)gEntList.FindEntityByName( NULL, m_creepMakerName );
+		if (maker->m_nMaxLiveChildren<=0) {
+			maker->m_nMaxLiveChildren = 10;
+		}
+		PropSetAnim( "Open" );
+		return true;
+	}
+	return false;
+}
+
+// Returns true if the bitmask for the specified lane was enabled. If no lanes were enabled, all will return true.
+BOOL DotaObjective::CheckLaneMode( ) { // Issue #24: AMP - 2013-10-04 - Conditionally open lanes
+	int laneMode = dotaLaneMode.GetInt();
+	if (laneMode<0) { // Default allow all lanes
+		return true;
+	}
+	const char * objectiveName = this->m_creepMakerName.ToCStr();
+	if (objectiveName==NULL) {
+		return false;
+	}
+
+	const char * laneToFind;
+	if (laneMode & 1) {
+		laneToFind = "center";
+	} else if (laneMode & 2) {
+		laneToFind = "left";
+	} else if (laneMode & 4) {
+		laneToFind = "right";
+	} else if (laneMode & 8) {
+		laneToFind = "other";
+	} else {
+		return false;
+	}
+	Msg("Verifying lane: %s contains %s -> %d\n", objectiveName, laneToFind, strstr(objectiveName, laneToFind)!=NULL);
+	return strstr(objectiveName, laneToFind)!=NULL;
 }
--- mp/src/game/server/hl2mp/dotaObjective.h
+++ mp/src/game/server/hl2mp/dotaObjective.h
@@ -13,10 +13,19 @@ public:
 
 	virtual int OnTakeDamage( const CTakeDamageInfo &info );
 
+	virtual int TakeAction( int dotaAction ); // Open/close the lane
+	virtual BOOL CheckLaneMode( ); // Determine if a lane is allowed to be opened
+
 	string_t m_creepMakerName;
 	string_t m_guardianName;
 
 	bool	m_bMet;
+
+	enum {
+		DOBJ_ACTION_NONE, // Do nothing
+		DOBJ_ACTION_OPEN, // Open objective door
+		DOBJ_ACTION_CLOSE // Close objective door
+	};
 
 protected:
 	int		m_timesHit;
